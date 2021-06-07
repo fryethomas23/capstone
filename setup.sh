d73053d8e5877647f6d370378de9a6b3164e0e21
Thomas Frye <fryethomas23@yahoo.com>
2021-06-07 09:50:21 -0500

    added procfile

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 
-Auth0_Domain_Name = 'fsnd-thomas..us.auth0.com'
+AUTH0_DOMAIN= 'fsnd-thomas..us.auth0.com'
 JWT_Signing_Secret = 'XByJz7evW-SGuopI_hBQ7bOqDk5Z9bmDv84EUexJXLoK8gEdPryDdjXnjZflMieN'
 Auth0 Client_Id = 'Z8jUbXQAB3Zs4rfUp7xiZfcrwo6bAeqW'
+API_AUDIENCE = 'soccer'
+DATABASE_URL
+TEST_AUTH
+ALGORITHMS
