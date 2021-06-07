87571546c96c8f23d1632c73d48328eb2ebf3129
Thomas Frye <fryethomas23@yahoo.com>
2021-06-07 11:03:47 -0500

    fixed env variables

--- config.py
+++ config.py
@@ -7,6 +7,9 @@ DB_NAME = os.getenv('DB_NAME',"capstone")
 DB_URL = os.getenv('DATABASE_URL',"postgresql+psycopg2://{}:{}@{}/{}".format(
     DB_USER, DB_PASSWORD, DB_HOST, DB_NAME))
 
+if DB_URL.startswith("postgres://"):
+    DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
+
 SQLALCHEMY_DATABASE_URI = DB_URL
 DEBUG = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
