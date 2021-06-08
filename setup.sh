c24dd2b78320fe8749cd5b040e71b92ce1be6886
Thomas Frye <fryethomas23@yahoo.com>
2021-06-08 06:57:13 -0500

    exported environment variables

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 
-AUTH0_DOMAIN = 'fsnd-thomas.us.auth0.com'
-JWT_Signing_Secret = 'XByJz7evW-SGuopI_hBQ7bOqDk5Z9bmDv84EUexJXLoK8gEdPryDdjXnjZflMieN'
-AUTH0_CLIENT_ID = 'Z8jUbXQAB3Zs4rfUp7xiZfcrwo6bAeqW'
-API_AUDIENCE = 'soccer'
+export AUTH0_DOMAIN = 'fsnd-thomas.us.auth0.com'
+export JWT_Signing_Secret = 'XByJz7evW-SGuopI_hBQ7bOqDk5Z9bmDv84EUexJXLoK8gEdPryDdjXnjZflMieN'
+export AUTH0_CLIENT_ID = 'Z8jUbXQAB3Zs4rfUp7xiZfcrwo6bAeqW'
+export API_AUDIENCE = 'soccer'
 DATABASE_URL
 TEST_AUTH
-ALGORITHMS
+export ALGORITHMS = ['RS256']
